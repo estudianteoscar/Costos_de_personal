40571f22f9b5826f4ad116a1aaa22537beb76865
estudianteoscar <od94847@outlook.es>
2023-11-30 13:17:42 -0500

    calcular_costos

---
+++ src/logica/Capa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+import Gui.Pantalla;
+
+/**
+ *
+ * @author strawtech
+ */
+public class Capa {
+    
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+    
+}
